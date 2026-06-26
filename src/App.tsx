9c4afeea876a442d3813b894ac4bbe566b31c242
nectar-tech <82286417+nectar-tech@users.noreply.github.com>
2026-06-26 14:06:55 +0300

    Simplify to basic React app and fix build

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,8 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import NeonOdysseyPage from './pages/NeonOdysseyPage'
-import AcademicsPage from './pages/AcademicsPage'
-import RimonPage from './pages/RimonPage'
-
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/neon-odyssey" element={<NeonOdysseyPage />} />
-        <Route path="/academics" element={<AcademicsPage />} />
-        <Route path="/rimon" element={<RimonPage />} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <h1>Welcome to Nectar Portfolio</h1>
+      <p>React app ready to build</p>
+    </div>
   )
 }
